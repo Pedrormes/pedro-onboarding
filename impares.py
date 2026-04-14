89beda80fc2c7865a2a684ff32e4a0822ea8cea7
Pedro Rodrigues Mesquita <143655818+Pedrormes@users.noreply.github.com>
2026-04-14 17:21:09 -0300

    Update impares.py
    Alterei para que não seja apenas mostrado os 10 primeiros ímpares, mas que tenha uma variável que retorne esses 10 primeiros números para a main.py.

--- impares.py
+++ impares.py
@@ -1,4 +1,8 @@
-for i in range(10):
-    print(2 * i + 1)
+def gerar_impares():
+    return [2 * i + 1 for i in range(10)]
 
- 
+
+
+if __name__ == "__main__":
+    for numero in gerar_impares():
+        print(numero)
